5dfd70dc40e6b2ec69b4baf073a2690662be2fb6
Nicole <ngott30@gmail.com>
2022-11-20 12:03:56 +0100

    finished p22

---
+++ Praktikum02/p22.asm
@@ -0,0 +1,18 @@
+
+org 100h
+
+; PRAKTIKUM 2: Aufgabe p22
+; Auf der LED-Reihe soll abwechselnd sichtbar zwischen den LEDs
+; 6-4-2-0 und 7-5-3-1 umgeschaltet werden
+
+start: MOV AX, 01010101b	; Wert in AX kopieren
+	   OUT 0,AX				; Wert in AX an LEDs ausgeben (hier: LEDs 6-4-2-0)
+	   CALL warte			; Aufruf Unterprogramm
+	   NOT AX				; Wert in AX invertieren
+	   OUT 0,AX				; Wert in AX an LEDs ausgeben (hier: LEDs 7-5-3-1)
+	   CALL warte
+	   jmp start
+	   
+warte: XOR CX,CX			; Warte-Unterprogramm
+	   schl: loop schl
+	   RET

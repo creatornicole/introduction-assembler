be9ae0d3f7f646a25c4a02eb7a934b909d19649a
Nicole <ngott30@gmail.com>
2022-12-09 12:49:07 +0100

    added comments to vorbereitungsaufgabe03

--- Praktikum04/vorbereitungsaufgabe03.asm
+++ Praktikum04/vorbereitungsaufgabe03.asm
@@ -4,18 +4,25 @@ org 100h	; Startpunkt Programm in Speicher
 ; PRAKTIKUM 4: Vorbereitungsaufgabe 3
 ; Erklären Sie die Funktion folgender Anweisung und Befehle:
 
-null: equ 3fh			
-stelle1: equ 92h
+null: equ 3fh			  ; sind Symboldefinitionen		
+stelle1: equ 92h		  ; weisen Ausdruck Namen zu (wie Konstantendefinition)
 
-	  mov cx,[var16]      ; Was wird jeweils geladen?
-	  mov bx,var8      
-	  mov al,[bx]
-	  mov [var8],al
-	  mov dx,stelle1
-	  mov al,null
-	  out dx,al
-	  or byte [0120h],00000011b ; byte: Warum? Effekt?
-	  move world [0200h],1      ; word: Warum? Effekt?
+						  ; Was wird jeweils geladen?
+	  mov cx,[var16]      ; (Direkte Adressierung) Laedt Operanden aus reservierten Speicherplatz var16
+						  ; hier: 1
+	  mov bx,var8         ; (Unmittelbare Adressierung) Laedt Adresse des reservierten Speicherplatzes var8
+						  ; hier: 120h
+	  mov al,[bx]	      ; (Indirekte Adressierung) Laedt Operanden der Adresse, die unter BX steht
+	  mov [var8],al		  ; (Direkte Adressierung) Kopiert Eintrag aus AL an reservierten Speicherplatz var8
+	  mov dx,stelle1	  ; (Unmittelbare Adressierung) Kopiert 92h in DX
+	  mov al,null		  ; (Unmittelbare Adressierung) Kopiert 3fh in AL
+	  out dx,al			  ; (Registeradressierung) Gibt Operand von AL an Port, der in DX steht, aus
 	  
-var8: db 0
-var16: dw 1
+	  or byte [0120h],00000011b ; byte: Warum? Effekt?
+								; byte gibt Aussage ueber Datentyp
+	  move word [0200h],1      ; word: Warum? Effekt?
+								; word gibt Aussage ueber Datentyp
+	  
+var8: db 0				  ; sind Datendefinitionen
+var16: dw 1				  ; reservieren Speicherplatz in Speicher
+						  ; ueber Namen auf diesen zugreifen (wie Variablenvereinbarung)
